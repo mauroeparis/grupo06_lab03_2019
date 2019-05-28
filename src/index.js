63084761a17259ac992ce531677b3c48089a10d3
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2019-05-28 15:30:43 -0300

    add navbar...not the best way to implement it maybe. but yeah. time will tell

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar } from 'react-bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossOrigin="anonymous"
+    />
+
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        {'MayWeather'}
+      </Navbar.Brand>
+    </Navbar>
+
+    <App />
+  </>, document.getElementById('root')
+);
 
 module.hot.accept();
