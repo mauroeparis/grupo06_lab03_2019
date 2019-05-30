9313205d74dd8ad08f048dba46ed7b7585a8190f
JuliBorre <borrejulieta@gmail.com>
2019-05-30 14:02:36 -0300

    add search bar and button, not working yet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import BoxLoader from "./Loader"
 import NavBar from "./NavBar"
+import SearchBar from "./SearchBar"
+import SearchButton from "./SearchButton"
 
 function App() {
   return (
     <>
       <NavBar />
+      <br />
+      <SearchBar />
+      <SearchButton />
       <BoxLoader />
     </>
   );
