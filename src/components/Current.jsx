b1143634c5e51b1de65492edd6d1ec042c46a1e4
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2019-06-08 00:10:03 -0300

    deleted few unused imports

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 
